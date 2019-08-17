cf1bd0f46cb6d2e4ca709ab35ae260e2ef9cf425
Mark Leo Sumadero <fryu771@gmail.com>
2019-08-17 17:57:28 +0800

    added help text support

---
+++ src/Components/HelpText/HelpText.js
@@ -0,0 +1,19 @@
+import className from 'classnames'
+
+export default class HelpText {
+    constructor(props) {
+        this.target = props.target
+        this.message = props.message
+
+        this.create()
+    }
+
+    create() {
+        let className = 'text-muted'
+        this.container = document.createElement('small')
+        this.container.className = className
+        this.container.innerHTML = this.message
+        //see https://gomakethings.com/how-to-insert-an-element-after-another-one-in-the-dom-with-vanilla-javascript/
+        this.target.after(this.container)
+    }
+}
