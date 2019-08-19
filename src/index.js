2041378b66f0f3b5c88a05ec6fd46ed3de9cde3f
Mark Leo Sumadero <fryu771@gmail.com>
2019-08-19 23:35:58 +0800

    added Calendar component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 export * from './Components/InputText'
 export * from './Components/Password'
 export * from './Components/HelpText'
 export * from './Components/ToolTip'
 export * from './Components/Forms/Form'
+export * from './Components/Calendar'
