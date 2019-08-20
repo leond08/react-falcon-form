0d5840823a79642e6ee6707a1a093a4761a3504c
Mark Leo Sumadero <fryu771@gmail.com>
2019-08-20 17:07:35 +0800

    updated dev

--- development/src/App.js
+++ development/src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react'
 import {Calendar, InputText} from 'react-falcon-form'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      value: ''
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -10,7 +17,7 @@ export default class App extends Component {
               <div className="col-sm">
                 <div>
                   <br /> <br />
-                  <Calendar />
+                  <Calendar value={this.state.value} onChange={e => this.setState({value: e.value})} />
                   <br /> <br />
                   <InputText />
                 </div>
